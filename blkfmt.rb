2a7e3837285ce82295d00f59ed228c9f7d154c3c
kt <kt@katbyte.me>
2018-10-31 23:48:27 -1000

    added count option

--- blkfmt.rb
+++ blkfmt.rb
@@ -16,113 +16,135 @@ BlockPair = Struct.new(:start, :finish, :desc) do
 
 end
 
-class BlkFmt
+class BlkReader
 
   @@pairs = [
-      BlockPair.new('```hcl',               '```', 'markdown'),
+      BlockPair.new('```hcl', '```', 'markdown'),
       BlockPair.new('return fmt.Sprintf(`', '`, ', 'markdown')
   ]
 
-  @count = 0
-
-  def initialize(mode, file=nil)
-    raise Thor::Error, 'unknown BlkFmt mode'.red unless  [:fmt, :diff, :count].include?(mode)
+  def initialize(mode, file = nil)
+    raise Thor::Error, 'ERROR unknown BlkFmt mode'.red unless [:fmt, :diff, :count].include?(mode)
     @mode = mode
+
+    @lines = 0
+    @lines_block = 0
+    @count = 0
   end
 
   def go
 
-    #current block pair we are in (not nil == buffering)
-    pair = nil
+    @buffer = [] #the current block
+    pair = nil  #current block pair we are in (not nil == buffering)
 
-    #the current working block
-    buffer = []
-
-    #for each line in file/stdin
     STDIN.each_line do |line|
+      @lines += 1
 
-      #if we are capturing data to format
-      if pair != nil
-
-        #check to see if we should stop
-        unless pair.finishes? line
-          #nope, add line to buffer and goto next line
-          buffer << line
+      if pair != nil #if we have started a pair and should buffer
+        @lines_block += 1
+        unless pair.finishes? line #check to see if we are at the end of a block
+          @buffer << line #if not buffer line and goto next
           next
         else
-          #we are finish capturing data, format it!
-          o, e, s = Open3.capture3("terraform fmt -", stdin_data: buffer.join(""))
 
+          #block done! so call block_read function
+          block_read(line)
 
-          #check exit status
-          if s.exitstatus == 0
-            #success! output it and the closing line
-            puts o
-          else
-            #have error, log it to stderr & output unformatted buffer
-            STDERR.puts e
-            puts buffer
-          end
-
-          #either way reset buffer and buffering value
-          buffer = []
+          #no reset the buffer/pair
+          @buffer = []
           pair = nil
-
-          #and output closing line
-          puts line
-          next
-
+          next #skip to next line
         end
       end
-
 
       #see if any pairs start here
       @@pairs.each do |p|
         if p.starts? line
+          @count += 1
           pair = p
           break
         end
       end
 
       #put starting line
-      puts line
+      line_read(line)
+    end
 
+    done
+  end
+
+  def run_format
+    return Open3.capture3("terraform fmt -", stdin_data: @buffer.join(""))
+  end
+
+  #after each line is read, default to output it (passthrough)
+  def line_read(line)
+    puts line
+  end
+
+  #block has been read ito buffer, line that finished the block is passed in
+  def block_read(line)
+    puts @buffer
+    puts line
+  end
+
+  def done
+
+  end
+end
+
+
+#format each block
+class BlkFmt < BlkReader
+  def line_read(line)
+    puts line
+  end
+
+  def block_read(line)
+    o, e, s = run_format
+
+    #check exit status
+    if s.exitstatus == 0
+      #success! output it and the closing line
+      puts o
+    else
+      #have error, log it to stderr & output unformatted buffer
+      STDERR.puts e
+      puts @buffer
+    end
+
+    puts line
+  end
+end
+
+
+class BlkCount < BlkReader
+
+  def initialize(mode, quiet, file = nil)
+    super(mode, file)
+    @quiet = quiet
+    @count_diff = 0
+  end
+
+  def line_read(line)
+
+  end
+
+  def block_read(line)
+    o, e, s = run_format
+
+    if s.exitstatus == 0
+      if o != @buffer.join("")
+        @count_diff += 1
+      end
+    else
+      STDERR.puts e
+    end
+  end
+
+  def done
+    if !@quiet || @count_diff > 0
+      puts "#{@count_diff} blocks require formatting (of #{@count})"
     end
   end
 end
-
-
-class TerraFmtBlocks < Thor
-
-  desc "fmt FILE", "format blocks of terraform found in FILE"
-  long_desc <<-LONGDESC
-  `terrafmt-blocks |fmt| FILE` will format blocks of terraform found in FILE
-
-      use --i FILE to read from stdin
-
-      > $ cat file | ./terrafmt-blocks -i
-  LONGDESC
-
-  option :stdin, type: :boolean, aliases: 'i'
-  def fmt(file=nil)
-    BlkFmt.new(:fmt, file).go
-  end
-
-  desc "diff", "show diff"
-  def count(name)
-    puts "Hello #{name}"
-  end
-
-  desc "count", "counts the number of blocks # (# requiring format)"
-  def count(name)
-    puts "Hello #{name}"
-  end
-
-
-
-  default_task :fmt
-end
-
-
-
-TerraFmtBlocks.start(ARGV)
--- terrafmt-blocks.rb
+++ terrafmt-blocks.rb
@@ -13,11 +13,7 @@ class TerraFmtBlocks < Thor
 
   desc "fmt FILE", "format blocks of terraform found in FILE"
   long_desc <<-LONGDESC
-  `terrafmt-blocks |fmt| FILE` will format blocks of terraform found in FILE
-
-      use --i FILE to read from stdin
-
-      > $ cat file | ./terrafmt-blocks -i
+  `terrafmt-blocks |fmt| |FILE|` will format blocks of terraform found in FILE or stdin if no file is specified
   LONGDESC
 
   option :stdin, type: :boolean, aliases: 'i'
@@ -25,17 +21,16 @@ class TerraFmtBlocks < Thor
     BlkFmt.new(:fmt, file).go
   end
 
-  desc "diff", "show diff"
-  def count(name)
-    puts "Hello #{name}"
+  desc "diff FILE", "will show a diff of what will be changed in the file"
+  def count(file=nil)
+    BlkFmt.new(:diff, file).go
   end
 
-  desc "count", "counts the number of blocks # (# requiring format)"
-  def count(name)
-    puts "Hello #{name}"
+  desc "count", "counts the number of blocks # and those generating a diff"
+  option :quiet, type: :boolean, aliases: 'q'
+  def count(file=nil)
+    BlkCount.new(:count, options[:quiet], file).go
   end
-
-
 
   default_task :fmt
 end
