f0bda15e0cd70e1d3cff54785d63a35a512c9609
magodo <wztdyl@sina.com>
2020-02-26 20:43:15 +0800

    no need to use panicwrap

--- lib/common/log.go
+++ lib/common/log.go
@@ -1,6 +1,10 @@
 package common
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
 	"os"
 
 	"github.com/sirupsen/logrus"
@@ -32,4 +36,55 @@ func createLogger() *logrus.Logger {
 	}
 
 	return l
+}
+
+func CaptureRun(f func() error) (stdout, stderr string, err error) {
+	// setup stdout
+	outpipeR, outpipeW, err := os.Pipe()
+	if err != nil {
+		return "", "", fmt.Errorf("failed to create pipe for stdout: %w", err)
+	}
+	outCh := make(chan string)
+	go func() {
+		buf := bytes.NewBufferString("")
+		io.Copy(buf, outpipeR)
+		// close read end, since have read everything out
+		outpipeR.Close()
+		outCh <- buf.String()
+	}()
+
+	// replace stdout
+	defer func(o *os.File) { os.Stdout = o }(os.Stdout)
+	os.Stdout = outpipeW
+
+	// setup stderr
+	errpipeR, errpipeW, err := os.Pipe()
+	if err != nil {
+		return "", "", fmt.Errorf("failed to create pipe for stderr: %w", err)
+	}
+	errCh := make(chan string)
+	go func() {
+		buf := bytes.NewBufferString("")
+		io.Copy(buf, errpipeR)
+		// close read end, since have read everything out
+		errpipeR.Close()
+		errCh <- buf.String()
+	}()
+
+	// replace stdout
+	defer func(o *os.File) { os.Stderr = o }(os.Stderr)
+	os.Stderr = errpipeW
+
+	// replace log writer
+	defer log.SetOutput(log.Writer())
+	log.SetOutput(errpipeR)
+
+	// invoke function
+	err = f()
+
+	// close pipe write end so that the copy routines could finish
+	outpipeW.Close()
+	errpipeW.Close()
+
+	return <-outCh, <-errCh, err
 }
--- lib/upgrade012/upgrade.go
+++ lib/upgrade012/upgrade.go
@@ -10,7 +10,9 @@ import (
 
 	"github.com/hashicorp/terraform/command"
 	"github.com/hashicorp/terraform/plugin"
+	"github.com/katbyte/terrafmt/lib/common"
 	"github.com/mitchellh/cli"
+	"github.com/spf13/viper"
 )
 
 func Block(b string) (string, error) {
@@ -52,13 +54,23 @@ func Block(b string) (string, error) {
 
 	upgradeCmd := &command.ZeroTwelveUpgradeCommand{Meta: meta}
 
-	// We specify "-force" here to allow a re-upgrade, otherwise it will complain the
-	// cfg has already been upgraded.
-	// Though, a re-upgrade will not always work, since it might failed to parse the 0.12 syntax
-	// using 0.11 syntax parser.
-	rc := upgradeCmd.Run([]string{"-yes", "-force", dir})
-	if rc != 0 {
-		return "", fmt.Errorf("upgrade to 0.12 failed (rc: %d): %s", rc, uiBuffer.String())
+	stdout, stderr, err := common.CaptureRun(func() error {
+		// We specify "-force" here to allow a re-upgrade, otherwise it will complain the
+		// cfg has already been upgraded.
+		// Though, a re-upgrade will not always work, since it might failed to parse the 0.12 syntax
+		// using 0.11 syntax parser.
+		rc := upgradeCmd.Run([]string{"-yes", "-force", dir})
+		if rc != 0 {
+			return fmt.Errorf("upgrade to 0.12 failed (rc: %d): %s", rc, uiBuffer.String())
+		}
+		return nil
+	})
+	if viper.GetBool("verbose") {
+		fmt.Fprintln(os.Stdout, stdout)
+		fmt.Fprintln(os.Stderr, stderr)
+	}
+	if err != nil {
+		return "", err
 	}
 
 	// Read from temp file
