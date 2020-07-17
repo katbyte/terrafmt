2cb1b584438916884487e9c0d16ac5847430690d
Graham Davison <gdavison@hashicorp.com>
2020-07-17 12:09:48 -0700

    Cleans up test failure output for error message tests

--- cli/cmds_test.go
+++ cli/cmds_test.go
@@ -2,15 +2,24 @@ package cli
 
 import (
 	"regexp"
-	"strings"
 	"testing"
 )
 
+var logMsgRegexp *regexp.Regexp
+
+func init() {
+	logMsgRegexp = regexp.MustCompile(".*msg=\"([^\"]+)\".*\n")
+}
+
 func checkExpectedErrors(t *testing.T, casename, errOutput string, expectedErrs []string) {
 	if expectedErrCount := len(expectedErrs); expectedErrCount > 0 {
-		actualErrs := strings.FieldsFunc(errOutput, func(c rune) bool {
-			return c == '\n'
-		})
+		allMatches := logMsgRegexp.FindAllStringSubmatch(errOutput, -1)
+		actualErrs := make([]string, 0, len(allMatches))
+		for _, matches := range allMatches {
+			if len(matches) == 2 {
+				actualErrs = append(actualErrs, matches[1])
+			}
+		}
 		if expectedErrCount != len(actualErrs) {
 			t.Errorf("Case %q: Expected %d error messages\n%#v,\ngot %d\n%#v", casename, expectedErrCount, expectedErrs, len(actualErrs), actualErrs)
 		} else {
@@ -20,7 +29,7 @@ func checkExpectedErrors(t *testing.T, casename, errOutput string, expectedErrs 
 					t.Fatalf("Case %q, error message %d: error parsing regexp: %s", casename, i+1, err)
 				}
 				if !match {
-					t.Errorf("Case %q: error message %d no match,\nexpected %q,\ngot      %q", casename, i+1, expectedErrs[i], actualErrs[i])
+					t.Errorf("Case %q: error message %d does not have match,\nexpected %q,\ngot      %q", casename, i+1, expectedErrs[i], actualErrs[i])
 				}
 			}
 		}
