1d4dc9478dc472b69204404079970c9295c3e371
Graham Davison <gdavison@hashicorp.com>
2020-08-10 17:31:46 -0700

    Removes rebase glitches

--- cli/upgrade012_test.go
+++ cli/upgrade012_test.go
@@ -205,7 +205,7 @@ func TestCmdUpgrade012StdinVerbose(t *testing.T) {
 	}
 }
 
-func TestCmdUpgrade012File(t *testing.T) {
+func TestCmdUpgrade012FileDefault(t *testing.T) {
 	testcases := []struct {
 		name       string
 		sourcefile string
@@ -401,97 +401,5 @@ func TestCmdUpgrade012FileVerbose(t *testing.T) {
 				t.Errorf("Unexpected summary:\nexpected %s\ngot      %s", expectedSummaryLine, summaryLine)
 			}
 		})
-	}
-}
-
-func TestCmdUpgrade012File(t *testing.T) {
-	testcases := []struct {
-		name       string
-		sourcefile string
-		resultfile string
-		noDiff     bool
-		errMsg     []string
-		fmtcompat  bool
-	}{
-		{
-			name:       "Go no change",
-			sourcefile: "testdata/no_diffs.go",
-			noDiff:     true,
-		},
-		{
-			name:       "Go formatting",
-			sourcefile: "testdata/has_diffs.go",
-			resultfile: "testdata/has_diffs_upgrade012.go", // This has stricter formatting than `fmt`
-		},
-		{
-			name:       "Go fmt verbs",
-			sourcefile: "testdata/fmt_compat.go",
-			noDiff:     true,
-			fmtcompat:  false,
-			errMsg: []string{
-				"block 1 @ testdata/fmt_compat.go:8 failed to process with: cmd.Run() failed in terraform init with exit status 1:",
-				"block 3 @ testdata/fmt_compat.go:26 failed to process with: cmd.Run() failed in terraform init with exit status 1:",
-			},
-		},
-		{
-			name:       "Go fmt verbs --fmtcompat",
-			sourcefile: "testdata/fmt_compat.go",
-			resultfile: "testdata/fmt_compat_upgrade012.go",
-			fmtcompat:  true,
-		},
-		{
-			name:       "Markdown no change",
-			sourcefile: "testdata/no_diffs.md",
-			noDiff:     true,
-		},
-		{
-			name:       "Markdown formatting",
-			sourcefile: "testdata/has_diffs.md",
-			resultfile: "testdata/has_diffs_upgrade012.md", // This has stricter formatting than `fmt`
-		},
-	}
-
-	fs := afero.NewCopyOnWriteFs(
-		afero.NewReadOnlyFs(afero.NewOsFs()),
-		afero.NewMemMapFs(),
-	)
-
-	for _, testcase := range testcases {
-		resultfile := testcase.resultfile
-		if testcase.noDiff {
-			resultfile = testcase.sourcefile
-		}
-		data, err := afero.ReadFile(fs, resultfile)
-		if err != nil {
-			t.Fatalf("Error reading test result file %q: %s", resultfile, err)
-		}
-		expected := c.String(string(data))
-
-		var outB strings.Builder
-		var errB strings.Builder
-		common.Log = common.CreateLogger(&errB)
-		_, err = upgrade012File(fs, testcase.sourcefile, testcase.fmtcompat, nil, &outB, &errB)
-		actualOut := outB.String()
-		actualErr := errB.String()
-
-		if err != nil {
-			t.Errorf("Case %q: Got an error when none was expected: %v", testcase.name, err)
-			continue
-		}
-
-		if actualOut != "" {
-			t.Errorf("Case %q: Stdout should not have output, got:\n%s", testcase.name, actualOut)
-		}
-
-		data, err = afero.ReadFile(fs, testcase.sourcefile)
-		if err != nil {
-			t.Fatalf("Error reading results from file %q: %s", resultfile, err)
-		}
-		actualContent := c.String(string(data))
-		if actualContent != expected {
-			t.Errorf("Case %q: File does not match expected:\n%s", testcase.name, diff.Diff(actualContent, expected))
-		}
-
-		checkExpectedErrors(t, testcase.name, actualErr, testcase.errMsg)
 	}
 }
