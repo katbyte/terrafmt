8389c686efe478cecae53ec33ad3341fc7a52297
kt <kt@katbyte.me>
2026-07-21 13:23:04 -0700

    fix versioning

--- lib/version/version.go
+++ lib/version/version.go
@@ -1,5 +1,15 @@
 package version
 
-const Version = "0.5.1"
+import "runtime/debug"
+
+var Version = "dev"
 
 var GitCommit string
+
+func init() {
+	if Version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" && info.Main.Version != "" {
+			Version = info.Main.Version
+		}
+	}
+}
